b66e3efa072ab02a527cc87ad24fcc0dfc711f99
92nishant <nishant.mathur92@gmail.com>
2024-01-17 23:07:36 +0530

    Test

--- server.js
+++ server.js
@@ -1,94 +1,94 @@
-require('dotenv-safe').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const compression = require('compression')
-const helmet = require('helmet')
-const cors = require('cors')
-const passport = require('passport')
-const app = express()
-const i18n = require('i18n')
-const initMongo = require('./config/mongo')
-const path = require('path')
-const fileUpload = require('express-fileupload')
+require("dotenv-safe").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
+const passport = require("passport");
+const app = express();
+const i18n = require("i18n");
+const initMongo = require("./config/mongo");
+const path = require("path");
+const fileUpload = require("express-fileupload");
 
 // Setup express server port from ENV, default: 3000
-app.set('port', process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
 // Enable only in development HTTP request logger middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 global.appRoot = path.resolve(__dirname);
-global.uploadDir = 'uploads/'
+global.uploadDir = "uploads/";
 // Redis cache enabled by env variable
-if (process.env.USE_REDIS === 'true') {
-  const getExpeditiousCache = require('express-expeditious')
+if (process.env.USE_REDIS === "true") {
+  const getExpeditiousCache = require("express-expeditious");
   const cache = getExpeditiousCache({
-    namespace: 'expresscache',
-    defaultTtl: '1 minute',
-    engine: require('expeditious-engine-redis')({
+    namespace: "expresscache",
+    defaultTtl: "1 minute",
+    engine: require("expeditious-engine-redis")({
       redis: {
         host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-      }
-    })
-  })
-  app.use(cache)
+        port: process.env.REDIS_PORT,
+      },
+    }),
+  });
+  app.use(cache);
 }
 
 // for parsing json
 app.use(
   bodyParser.json({
-    limit: '20mb'
+    limit: "20mb",
   })
-)
+);
 // for parsing application/x-www-form-urlencoded
 app.use(
   bodyParser.urlencoded({
-    limit: '20mb',
-    extended: true
+    limit: "20mb",
+    extended: true,
   })
-)
+);
 
 // i18n
 i18n.configure({
-  locales: ['en', 'es'],
+  locales: ["en", "es"],
   directory: `${__dirname}/locales`,
-  defaultLocale: 'en',
-  objectNotation: true
-})
-app.use(i18n.init)
-app.use(fileUpload())
+  defaultLocale: "en",
+  objectNotation: true,
+});
+app.use(i18n.init);
+app.use(fileUpload());
 // Init all other stuff
-app.use(cors())
-app.use(passport.initialize())
-app.use(compression())
-app.use(helmet())
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, 'views'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-app.use(require('./app/routes'))
+app.use(cors());
+app.use(passport.initialize());
+app.use(compression());
+app.use(helmet());
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(require("./app/routes"));
 
-const server = app.listen(app.get('port'))
+const server = app.listen(app.get("port"));
 
-let io = require("socket.io")(server,{
+let io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:8080",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   global.socket = socket;
-  global.socket.on('userAdded', (data) => {
-    console.log(data)
-  })
-  console.log(`New Connection: ${socket.id}`)
-})
+  global.socket.on("userAdded", (data) => {
+    console.log(data);
+  });
+  console.log(`New Connection: ${socket.id}`);
+});
 
 // Init MongoDB
-initMongo()
+initMongo();
 
-module.exports = app // for testing
+module.exports = app; // for testing
